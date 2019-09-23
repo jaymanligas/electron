8aa458d453cb19de68a8330049762cdfed9aaeb4
Vergie Mujemulta <jay.manligas@gmail.com>
2019-09-24 05:44:03 +0800

    able to write

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
+const $ = require("jquery");
 const fs = require("fs");
 const path = require("path");
-
+theForm = $("form");
 btnSubmit = document.getElementById("submit");
 company = document.getElementById("company");
-name = document.getElementById("name");
+namex = document.getElementById("namex");
 designation = document.getElementById("designation");
 email = document.getElementById("email");
 
 filename = "registered.csv";
 pathName = path.join(__dirname, "Files");
 
+theForm.on("submit", function(e) {
+  e.preventDefault();
+  console.log(e);
+});
+
 btnSubmit.addEventListener("click", function() {
   let file = path.join(pathName, filename);
-  let contents =
-    contents +
-    fs.readFile(file, function(err, data) {
+  fs.readFile(file, function(err, data) {
+    if (!!err) {
+      console.log("no file");
+      data = "";
+    }
+    let contents =
+      data +
+      `${namex.value},${company.value},${designation.value},${email.value}\n`;
+    fs.writeFile(file, contents, function(err) {
       if (!!err) {
-        console.log("no file");
-        data = "";
+        return console.error(err);
       }
-      let contents =
-        data +
-        `${name.value}\t${company.value}\t${designation.value}\t${email.value}\n`;
-      fs.writeFile(file, contents, function(err) {
-        if (!!err) {
-          return console.error(err);
-        }
-        console.log("created");
-      });
+      console.log("created");
     });
+  });
 });
